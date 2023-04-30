e027ebba71ab6c3d95079f6b3addce876cfbe8d1
moeinpourvosoughi <131144770+moeinpourvosoughi@users.noreply.github.com>
2023-04-30 14:57:01 +0430

    2023 . 4 . 30

--- apps/Anbardari/views.py
+++ apps/Anbardari/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from .models import *
 
-# Create your views here.
+
+
+class ProductView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request):
+        queryset = ProductsModel.objects.all()
+        serializers = ProductSerializers(queryset, many=True)
+        return Response(serializers.data, status=status.HTTP_200_OK)
+
+
+    def post(self, request):
+        data = request.data
+        Name = data['name']
+        Price = data['price']
+        Number = data['number']
+        Expire_date = data['expire_date']
+        Product = ProductsModel(name=Name, price=Price, number=Number, expire_date=Expire_date)
+        Product.save()
+        return Response(status=status.HTTP_200_OK)
+
+
+    def delete(self, request):
+        data = request.data
+        product = ProductsModel.objects.get(name=data['name'])
+        product.delete()
+        return Response(status=status.HTTP_200_OK)
+
+
+       
+    def put(self, request):
+        data = request.data
+        product = ProductsModel.objects.get(pk=data['id'])
+        product.name = data['name']
+        product.price = data['price']
+        product.number = data['number']
+        product.add_date = data['AddDate']
