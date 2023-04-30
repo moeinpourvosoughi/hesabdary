2d04502f2d85421c138f2358b1a30449696e9653
moeinpourvosoughi <131144770+moeinpourvosoughi@users.noreply.github.com>
2023-04-30 14:56:07 +0430

    test2

--- apps/Anbardari/admin.py
+++ apps/Anbardari/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Products)
-admin.site.register(Gozaresh)
+admin.site.register(ProductsModel)
+admin.site.register(GozareshModel)
--- apps/Anbardari/models.py
+++ apps/Anbardari/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from rest_framework import serializers
 
+# Models
 
-class Products(models.Model):
-     name = models.CharField(max_length=225)
-     price = models.FloatField()
+class ProductsModel(models.Model):
+     name = models.CharField(max_length=225,unique=True)
+     price = models.IntegerField(help_text= 'تاریخ تولید ')
      number = models.IntegerField()
      add_date=models.DateTimeField(auto_now_add=True)
      expire_date=models.DateTimeField()
@@ -11,8 +13,30 @@ class Products(models.Model):
      def __str__(self):
           return self.name
 
-class Gozaresh(models.Model):
-    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+class GozareshModel(models.Model):
+    product = models.ForeignKey(ProductsModel, on_delete=models.CASCADE)
+    def __str__(self):
+          return self.product.name
+
+# Serializers
+
+class ProductSerializers(serializers.ModelSerializer):
+     
+     class Meta:
+          model = ProductsModel
+          fields = ['name', 'price', "number", 'add_date', 'expire_date']
+          # depth=1
+     # def to_representation(self, instance):
+     #      data=super().to_representation(instance)
+     #      data['tese']=6748765
+     #      return data
+
+class GozareshSerializers(serializers.ModelSerializer):
+     class Meta:
+          model = GozareshModel
+          fields = ['product']
+          depth = 1
+# 
 
 
 
